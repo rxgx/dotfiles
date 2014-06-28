2131fc2c2e2a2475caba10cd4fabb1c208305bcf
Ryan Gasparini <ryan@rxgx.com>
2014-06-28 01:28:32 -0700

    Updated the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-read -p "Press start to continue..."
-cp ./.gemrc ~/
-cp ./.gitconfig ~/
-cp ./.gitignore ~/
-cp ./.profile ~/
+read -p '%C(yellow)This will overwrite your existing dotfiles!%C(reset) Press start to continue...'
+cp ./gemrc ~/
+cp ./gitconfig ~/
+cp ./gitignore ~/
+cp ./profile ~/
 source ~/.profile

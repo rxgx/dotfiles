56649dfa6ef641412d8d7888739885ada592269d
Ryan Gasparini <ryan@rxgx.com>
2020-05-10 17:20:41 -0700

    Add config for Volta

---
+++ files/.zsh/volta.zsh
@@ -0,0 +1,5 @@
+# tabtab source for packages
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
+export VOLTA_HOME="/Users/rxgx/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"

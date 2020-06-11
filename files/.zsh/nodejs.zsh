6cac4c7ba319613d13eb05c703f7a4512f7633f7
Ryan Gasparini <ryan@rxgx.com>
2020-06-11 11:58:49 -0700

    Update and rename nvm.zsh to nodejs.zsh

---
+++ files/.zsh/nodejs.zsh
@@ -0,0 +1,5 @@
+# Set development as the default environment
+export NODE_ENV=development
+
+alias npmlsg='npm list -g --depth=0'
+alias npmlsl='npm list -g --depth=0 --link=true'
--- files/.zsh/nvm.zsh
+++
@@ -1,9 +0,0 @@
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-# Node Environment
-export NODE_ENV=development
-
-alias npmlsg='npm list -g --depth=0'
-alias npmlsl='npm list -g --depth=0 --link=true'

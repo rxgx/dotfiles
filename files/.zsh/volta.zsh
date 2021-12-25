c287e02d7dcfc19bc926ec8ad7d542f825005748
Ryan Gasparini <ryan@rxgx.com>
2021-12-24 18:24:54 -0700

    Update volta.zsh
    remove static rxgx directory

--- files/.zsh/volta.zsh
+++ files/.zsh/volta.zsh
@@ -1,5 +1,5 @@
 # tabtab source for packages
 # uninstall by removing these lines
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
-export VOLTA_HOME="/Users/rxgx/.volta"
+export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
